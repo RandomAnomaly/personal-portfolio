e71470bf0748cd41263bb86ddc1ee283ae4b6e54
Jack Gammon <Jack Gammon>
2016-08-13 21:19:09 +1200

    Added images to each screen

--- public_html/laptopanims.js
+++ public_html/laptopanims.js
@@ -8,14 +8,15 @@ var animLength = 1000;
 
 $(document).ready(function () {
     // Apply page piping
-    $('#pagepiling').pagepiling();
+    $('#pagepiling').pagepiling({
+
+    });
 
     var paths = document.querySelectorAll('#section1 path');
 
     for (var i = 0; i < paths.length; i++) {
         var element = paths[i];
         var length = element.getTotalLength();
-        console.log(length);
         element.style.strokeDasharray = length;
         element.style.strokeDashoffset = length;
     }
