be784326e99d195a51bf603150e1996e7f1e841d
jack <jack.gammon@equinox.co.nz>
2016-08-15 10:06:53 +1200

    Made laptop animation trigger on section switch

--- public_html/anims.js
+++ public_html/anims.js
@@ -4,26 +4,27 @@
  * and open the template in the editor.
  */
 
-var animLength = 1000;
 
 $(document).ready(function () {
+    clearLaptop();
     // Apply page piling
     $('#pagepiling').pagepiling({
+        
+        onLeave: function(index, nextIndex, direction){
+            
+            // enter latop screen
+            if(nextIndex === 2){
+                animateInLaptop();
+            }
+            // leave laptop screen
+            if(index === 2){
+                clearLaptop();
+            }
+        }
     });
 
-    var paths = document.querySelectorAll('#section2 path');
-
-    for (var i = 0; i < paths.length; i++) {
-        var element = paths[i];
-        var length = element.getTotalLength();
-        element.style.strokeDasharray = length;
-        element.style.strokeDashoffset = length;
-    }
-
-    var i = 0;
-
     var orangeLight = document.querySelectorAll('.statusLightsOrange');
-    window.setInterval(flashLight(orangeLight, 'orange'), 1000);
+    //window.setInterval(flashLight(orangeLight, 'orange'), 1000);
     
     var whiteLight = document.querySelector('.statusLightsWhite');
     window.setInterval(flashLight(whiteLight, 'white'),500);
@@ -36,12 +37,35 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-
 function flashLight(element, color) {
     return function () {
         var e = element;
         var c = color;
         (e.style.fill === 'black') ? e.style.fill = c : e.style.fill = 'black';
     };
+}
+
+function clearLaptop() {
+    
+    var paths = document.querySelectorAll('#section2 path');
+
+    for (var i = 0; i < paths.length; i++) {
+        var element = paths[i];
+        var length = element.getTotalLength();
+        
+        element.style.transition = "stroke-dashoffset 0.5s linear";
+        element.style.strokeDasharray = length;
+        element.style.strokeDashoffset = length;
+    }
+}
+
+function animateInLaptop() {
+    
+    var paths = document.querySelectorAll('#section2 path');
+
+    for (var i = 0; i < paths.length; i++) {
+        var element = paths[i];
+        element.style.transition = "stroke-dashoffset 2s linear";
+        element.style.strokeDashoffset = 0;
+    }
 }
