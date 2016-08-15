2c96bc49f52203b8a450bd8692b3158d6edafac1
jack <jack.gammon@equinox.co.nz>
2016-08-15 16:40:25 +1200

    Moved laptop display logic into namespace

--- public_html/anims.js
+++ public_html/anims.js
@@ -15,6 +15,7 @@ var display = {
             pathStyles.strokeDasharray = styles.array;
         }
     },
+    
     applyStyleToSvgPathArray: function (pathArray, style) {
         for (var i = 0; i < pathArray.length; i += 1) {
             var element = pathArray[i];
@@ -23,8 +24,47 @@ var display = {
     }
 };
 
+var laptop = {
+    flashLight: function (element, color) {
+        return function () {
+            var e = element;
+            var c = color;
+            (e.style.fill === 'black') ? e.style.fill = c : e.style.fill = 'black';
+        };
+    },
+    
+    clearLaptop: function () {
+        var paths = document.querySelectorAll('#section2 path');
+        for (var i = 0; i < paths.length; i++) {
+            var element = paths[i];
+            var length = element.getTotalLength();
+            var styles = {transition: "stroke-dashoffset 0.5s linear",
+                offset: length, array: length};
+            display.applyStyleToSvgPath(element, styles);
+        }
+    },
+    
+    animateInLaptop: function () {
+        var paths = document.querySelectorAll('#section2 path');
+        var style = {transition: "stroke-dashoffset 1s linear", offset: 0};
+        display.applyStyleToSvgPathArray(paths, style);
+    },
+    
+    initialiseLaptop: function () {
+        var orangeLight = document.querySelector('.statusLightsOrange');
+        window.setInterval(laptop.flashLight(orangeLight, 'orange'), 1000);
+
+        var whiteLight = document.querySelector('.statusLightsWhite');
+        window.setInterval(laptop.flashLight(whiteLight, 'white'), 500);
+
+        var greenLight = document.querySelector('.statusLightsGreen');
+        window.setInterval(laptop.flashLight(greenLight, 'green'), 1500);
+    }
+};
+
 $(document).ready(function () {
-    clearLaptop();
+    laptop.initialiseLaptop();
+    laptop.clearLaptop();
     clearTachy();
     // Apply page piling
     $('#pagepiling').pagepiling({
@@ -32,11 +72,11 @@ $(document).ready(function () {
 
             // enter latop screen
             if (nextIndex === 2) {
-                animateInLaptop();
+                laptop.animateInLaptop();
             }
             // leave laptop screen
             if (index === 2) {
-                clearLaptop();
+                laptop.clearLaptop();
             }
 
             //enter tachy screen
@@ -50,40 +90,8 @@ $(document).ready(function () {
         }
     });
 
-    var orangeLight = document.querySelector('.statusLightsOrange');
-    window.setInterval(flashLight(orangeLight, 'orange'), 1000);
 
-    var whiteLight = document.querySelector('.statusLightsWhite');
-    window.setInterval(flashLight(whiteLight, 'white'), 500);
-
-    var greenLight = document.querySelector('.statusLightsGreen');
-    window.setInterval(flashLight(greenLight, 'green'), 1500);
 });
-
-function flashLight(element, color) {
-    return function () {
-        var e = element;
-        var c = color;
-        (e.style.fill === 'black') ? e.style.fill = c : e.style.fill = 'black';
-    };
-}
-
-function clearLaptop() {
-    var paths = document.querySelectorAll('#section2 path');
-    for (var i = 0; i < paths.length; i++) {
-        var element = paths[i];
-        var length = element.getTotalLength();
-        var styles = {transition: "stroke-dashoffset 0.5s linear",
-            offset: length, array: length};
-        display.applyStyleToSvgPath(element, styles);
-    }
-}
-
-function animateInLaptop() {
-    var paths = document.querySelectorAll('#section2 path');
-    var style = {transition: "stroke-dashoffset 1s linear", offset: 0};
-    display.applyStyleToSvgPathArray(paths, style);
-}
 
 function clearTachy() {
     var paths = document.querySelectorAll('#section3 path');
